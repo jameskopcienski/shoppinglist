d6592ea3f3c9b0697f256d97665f73288925ddc1
James Kopcienski <jkopcienski@gmail.com>
2013-06-10 11:48:14 -0400

    question to group

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   
+  $('#addItem').click(addNewItem);
+  $("#newItem").keyup(function(event){
+    if(event.keyCode == 13){
+      $("#addItem").click();
+    }
+  });
   
+  function addNewItem() {
+    var itemText = $('#newItem').val();
+    $('#itemList').append("<li><form><input name='listItem' type='checkbox'><span>" + itemText + "</span></form></li>");
+  }
+  
+  $(":checkbox").change(function() {
+    $(this).siblings().toggleClass('checked');
+  });
   
 });
+
